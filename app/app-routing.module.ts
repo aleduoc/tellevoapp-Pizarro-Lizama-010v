9f4ea7101d5700bfb894854a26e0604f03fc4c78
Nicolas Lizama <nicolasdraven91@gmail.com>
2023-08-30 23:40:21 -0400

    ToyAsustao

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -10,7 +10,8 @@ const routes: Routes = [
   {
     path: 'inicio',
     loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
-  },  {
+  },
+  {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
@@ -18,16 +19,11 @@ const routes: Routes = [
     path: 'tipo-user',
     loadChildren: () => import('./pages/tipo-user/tipo-user.module').then( m => m.TipoUserPageModule)
   },
-<<<<<<< HEAD
-=======
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   
-];
->>>>>>> a8aca725e8cb0970a79b9164a287bc781620a02c
-
 ];
 
 @NgModule({
