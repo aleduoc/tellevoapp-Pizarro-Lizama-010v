d9db92898ae17ffccb0b06554d48e95269e54ab9
Alexander Pizarro <alex.pizarroa@duocuc.cl>
2023-11-14 04:36:07 -0300

    ruta trazadaaa

--- app/pages/viajarconductor/viajarconductor.page.ts
+++ app/pages/viajarconductor/viajarconductor.page.ts
@@ -1,12 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
-import { AlertController } from '@ionic/angular';
+// viajarconductor.page.ts
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MenuController, ToastController } from '@ionic/angular';
 import { AuthService } from 'src/app/servicios/auth.service';
-import { ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { GoogleMapsService } from 'src/app/servicios/google-map.service';
-
-declare var google: any;
 
 @Component({
   selector: 'app-viajarconductor',
@@ -14,74 +11,86 @@ declare var google: any;
   styleUrls: ['./viajarconductor.page.scss'],
 })
 
-
 export class ViajarconductorPage implements OnInit {
-  usuario={
-    email:"",
+  usuario = {
+    email: "",
   }
+
+  @ViewChild('ubicacionInput') ubicacionInput: any;
+
   mostrarCarga: boolean = false;
 
   lat = 0; // Latitud inicial
   lng = 0; // Longitud inicial
 
   constructor(private menuController: MenuController,
-              private alertController: AlertController,
-              public authservice: AuthService,
-              private toastcontroller: ToastController,
-              private googleMapsService: GoogleMapsService,
-              private router: Router) { this.obtainStorage(); }
+    public authservice: AuthService,
+    private toastcontroller: ToastController,
+    private googleMapsService: GoogleMapsService,
+    private router: Router) { this.obtainStorage(); }
 
   ngOnInit(): void {
     this.googleMapsService.obtenerUbicacionActual()
       .then(({ lat, lng }) => {
         this.lat = lat;
         this.lng = lng;
-        this.googleMapsService.initMap(lat, lng, 'map');
+        this.googleMapsService.initAutocompleteAndDirectionMap('map', { lat, lng });
       })
       .catch((error) => {
         console.error('Error al obtener la ubicación:', error);
       });
   }
 
-  obtainStorage(){
+  obtainStorage() {
     let email = sessionStorage.getItem("email");
-     
+
     if (email) {
       this.usuario.email = email;
     }
   }
 
   logout() {
-    this.authservice.logoutUser(); // Llama al método logout del servicio
+    this.authservice.logoutUser();
     this.router.navigate(['/inicio']);
     this.showToast('Se ha cerrado sesión');
   }
 
-  async showToast(msg:any){
-    const toast= await this.toastcontroller.create({
+  MostrarMenu() {
+    this.menuController.open('first');
+  }
+
+  async trazarRuta() {
+    const inputElement = this.ubicacionInput.el.querySelector('input');
+    inputElement.blur();
+
+    const destination = inputElement.value;
+
+    if (!destination) {
+      this.showToast('Por favor, selecciona una dirección.');
+      return;
+    }
+
+    const result = await this.googleMapsService.obtenerCoordenadasDireccion(destination);
+
+    if (result) {
+      const origin = { lat: this.lat, lng: this.lng };
+      this.googleMapsService.trazarRuta('map', origin, result);
+    } else {
+      this.showToast('No se pudo obtener la coordenada de la dirección.');
+    }
+  }
+
+  async showToast(msg: any) {
+    const toast = await this.toastcontroller.create({
       message: msg,
       duration: 3000
     })
     toast.present();
   }
-
-  MostrarMenu(){
-    this.menuController.open('first');
-  }
-
-  Viajar() {
-    this.mostrarCarga = true;
-    setTimeout(() => {
-      this.mostrarCarga = false;
-    }, 4000);
-  }
-
-
-  
-
-  
-  
-
-
 }
+
+  
+  
+
+
 
--- app/servicios/google-map.service.ts
+++ app/servicios/google-map.service.ts
@@ -1,3 +1,4 @@
+// google-map.service.ts
 import { Injectable } from '@angular/core';
 import { environments } from 'src/environments/environment';
 
@@ -9,10 +10,12 @@ declare var google: any;
 })
 export class GoogleMapsService {
   private apiKey: string;
+  private marker: any; 
 
   constructor() {
     // Obtener la API Key desde el environment
     this.apiKey = environments.googleMapsApiKey;
+    this.marker = new google.maps.Marker();
   }
 
   obtenerUbicacionActual(): Promise<{ lat: number, lng: number }> {
@@ -50,4 +53,88 @@ export class GoogleMapsService {
     });
   }
 
+  // Nuevo método para inicializar autocompletado y trazar línea
+  initAutocompleteAndDirectionMap(elementId: string, origin: { lat: number, lng: number }): void {
+    const map = new google.maps.Map(document.getElementById(elementId), {
+      center: { lat: origin.lat, lng: origin.lng },
+      zoom: 15
+    });
+
+    const marker = new google.maps.Marker({
+      position: { lat: origin.lat, lng: origin.lng },
+      map: map,
+      title: 'Mi Ubicación',
+      label: {
+        text: 'Estás aquí',
+        color: 'black',
+        fontWeight: 'bold'
+      }
+    });
+
+    const autocompleteInput = document.getElementById('ubicacion');
+    const autocomplete = new google.maps.places.Autocomplete(autocompleteInput, {
+      fields: ["address_components", "geometry", "name"],
+      types: ["address"],
+    });
+
+    autocomplete.addListener('place_changed', () => {
+      const destination = autocomplete.getPlace();
+      if (!destination.geometry) {
+        window.alert('No details available for input: \'' + destination.name + '\'');
+        return;
+      }
+
+      // Renderizar dirección y trazar línea
+      this.renderAddressAndDirection(map, marker.getPosition(), destination.geometry.location);
+    });
+  }
+
+  obtenerCoordenadasDireccion(direccion: string): Promise<{ lat: number, lng: number }> {
+    return new Promise((resolve, reject) => {
+      const geocoder = new google.maps.Geocoder();
+      geocoder.geocode({ address: direccion }, (results: any, status: any) => {
+        if (status === 'OK') {
+          const location = results[0].geometry.location;
+          resolve({ lat: location.lat(), lng: location.lng() });
+        } else {
+          reject('No se pudo obtener la coordenada de la dirección.');
+        }
+      });
+    });
+  }
+
+  trazarRuta(mapId: string, origin: { lat: number, lng: number }, destination: { lat: number, lng: number }): void {
+    const map = new google.maps.Map(document.getElementById(mapId), {
+      center: { lat: origin.lat, lng: origin.lng },
+      zoom: 15
+    });
+
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+    directionsRenderer.setMap(map);
+
+    const request = {
+      origin: origin,
+      destination: destination,
+      travelMode: 'DRIVING'
+    };
+
+    directionsService.route(request, (result: any, status: any) => {
+      if (status === 'OK') {
+        directionsRenderer.setDirections(result);
+      } else {
+        console.error('Error al trazar la ruta:', status);
+      }
+    });
+  }
+
+  private renderAddressAndDirection(map: any, origin: any, destination: any): void {
+    // Renderizar dirección
+    map.setCenter(destination);
+    this.marker.setPosition(destination);
+    this.marker.setVisible(true);
+
+    // Trazar línea entre origen y destino
+    this.trazarRuta('map', origin, destination);
+  }
 }
